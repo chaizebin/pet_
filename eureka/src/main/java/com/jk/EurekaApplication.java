0e2118607c87fa74b0c65b24382e5fc5be42a856
yuanshuoshuo <987177067@qq.com>
2019-05-22 09:06:06 +0800

    ....

--- eureka/src/main/java/com/jk/EurekaApplication.java
+++ eureka/src/main/java/com/jk/EurekaApplication.java
@@ -4,12 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaApplication {
 
     public static void main(String[] args) {
+    /*    int newNum = (int)((Math.random()*33-1));
+        System.out.println(newNum);*/
         SpringApplication.run(EurekaApplication.class, args);
     }
+
 
 }
